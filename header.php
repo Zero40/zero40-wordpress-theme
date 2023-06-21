50e92cd4aba89e15277060a7720993c136aec3d5
viniman <vinicius.carlos@live.com>
2023-06-21 02:43:48 -0300

    fix navbar items to not wrap when screen decreases below 1200px

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Header section for our theme
  *
@@ -18,8 +19,9 @@
 <!--[if !(IE 7) | !(IE 8) ]><!-->
 <html <?php language_attributes(); ?>>
 <!--<![endif]-->
+
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="http://gmpg.org/xfn/11">
 
@@ -29,75 +31,65 @@
     <script src="<?php echo get_stylesheet_directory_uri() ?>/js/tagify.js"></script>
     <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri() ?>/css/tagify.css">
 
-    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
     <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 
-        <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container-fluid">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-ex-collapse">
+                        <span class="sr-only"><?php _e('Toggle navigation', 'integral'); ?></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
 
-            <div class="container-fluid">
+                    <?php
+                    $integral_logo = get_theme_mod('integral_logo');
 
-                <div class="container">
+                    if (isset($integral_logo) && $integral_logo != "") :
+                        echo '<h1 class="site-title"><a href="' . esc_url(home_url('/')) . '" class="navbar-brand">';
+                        echo '<img class="img-responsive" src="' . $integral_logo . '" alt="' . get_bloginfo('title') . '">';
+                        echo '</a></h1>';
+                    else :
+                        echo '<h1 class="site-title"><a href="' . esc_url(home_url('/')) . '" title="' . get_bloginfo('title') . '" class="navbar-brand">';
+                        echo '' . get_bloginfo('title') . '';
+                        echo '</a></h1>';
+                    endif;
+                    ?>
+                </div>
 
-                    <div class="navbar-header">
+                <?php if (has_nav_menu('primary')) : ?>
 
-                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-ex-collapse">
+                    <?php
+                    $navbar_theme = wp_nav_menu(
+                        array(
+                            'menu'              => 'primary',
+                            'theme_location'    => 'primary',
+                            'depth'             => 3,
+                            'container'         => 'div',
+                            'container_class'   => 'collapse navbar-collapse',
+                            'container_id'      => 'navbar-ex-collapse',
+                            'menu_class'        => 'nav navbar-nav navbar-right navbar-expand-lg d-flex justify-content-end',
+                            'echo'              => false,
+                            'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
+                            'walker'            => new wp_bootstrap_navwalker()
+                        )
+                    );
 
-                            <span class="sr-only"><?php _e( 'Toggle navigation', 'integral' ); ?></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
-                            <span class="icon-bar"></span>
+                    $navbar_theme = str_replace('menu-item ', 'ml-4 ml-lg-5 menu-item ', $navbar_theme);
+                    print_r( $navbar_theme);
 
-                        </button>
+                    ?>
 
-                        <?php
-
-                            $integral_logo = get_theme_mod('integral_logo');
-
-                            if(isset($integral_logo) && $integral_logo != ""):
-
-                                echo '<h1 class="site-title"><a href="'.esc_url( home_url( '/' ) ).'" class="navbar-brand">';
-
-                                    echo '<img class="img-responsive" src="'.$integral_logo.'" alt="'.get_bloginfo('title').'">';
-
-                                echo '</a></h1>';
-
-                            else:
-
-                                echo '<h1 class="site-title"><a href="'.esc_url( home_url( '/' ) ).'" title="'.get_bloginfo('title').'" class="navbar-brand">';
-
-                                    echo ''.get_bloginfo('title').'';
-
-                                echo '</a></h1>';
-
-                            endif;
-
-                        ?>
-
-                    </div>
-
-                    <?php if ( has_nav_menu( 'primary' ) ) : ?>
-
-                        <?php
-                            wp_nav_menu( array(
-                                'menu'              => 'primary',
-                                'theme_location'    => 'primary',
-                                'depth'             => 3,
-                                'container'         => 'div',
-                                'container_class'   => 'collapse navbar-collapse',
-                                'container_id'      => 'navbar-ex-collapse',
-                                'menu_class'        => 'nav navbar-nav navbar-right navbar-expand-lg',
-                                'fallback_cb'       => 'wp_bootstrap_navwalker::fallback',
-                                'walker'            => new wp_bootstrap_navwalker())
-                            );
-                        ?>
-
-                    <?php endif; ?>
-
-               </div>
+                <?php endif; ?>
 
             </div>
 
-        </nav>
+        </div>
+
+    </nav>
